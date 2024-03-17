d812bc663f4ff485edd544749df012eb188f97dd
REEZNN <johanstation15@gmail.com>
2024-03-17 03:04:10 -0300

    a

--- block/src/App.jsx
+++ block/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Menu from "./Menu.jsx";
 import Bannerp from "./Bannerp.jsx";
+import TendenciasHoy from "./TendenciasHoy.jsx"
 
 function App() {
   return (
     <div>
       <Menu />
       <Bannerp/>
+      <TendenciasHoy/>
     </div>
   );
 }
--- block/src/TendenciasHoy.jsx
+++ block/src/TendenciasHoy.jsx
@@ -1,6 +1,45 @@
-import "./Css/TendenciasHoy.css"
+import "./Css/TendenciasHoy.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const TendenciasHoy = () => {
-  return <div></div>;
+  const [tendencias, setTendencias] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const options = {
+      headers: {
+        accept: "application/json",
+        Authorization: "Bearer 2da173810187d9446d4b38d39c6e1569",
+      },
+    };
+
+    axios
+      .get(
+        "https://api.themoviedb.org/3/trending/all/day?api_key=2da173810187d9446d4b38d39c6e1569",
+        options
+      )
+      .then((response) => {
+        setTendencias(response.data.results);
+        console.log(response.data.results);
+      })
+      .catch(() => {
+        setError("Hubo un error al cargar las Tendencias de Hoy");
+      });
+  }, []);
+
+  if (error) {
+    return <h2 className="title">{error}</h2>;
+  }
+
+  if (!tendencias) {
+    return <h2 className="title">Cargando Tendencias de Hoy...</h2>;
+  }
+
+  return (
+    <div>
+      <h2 className="">Tendencias Hoy</h2>
+    </div>
+  );
 };
 export default TendenciasHoy;

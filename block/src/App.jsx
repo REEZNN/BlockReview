39839ddd1a0c1b1f9cf72279bd3195354810f455
REEZNN <johanstation15@gmail.com>
2024-03-18 23:57:54 -0300

    responsive menu

--- block/src/App.jsx
+++ block/src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
 import Menu from "./Menu.jsx";
-import Bannerp from "./Bannerp.jsx";
-import TendenciasHoy from "./TendenciasHoy.jsx"
-
+//import Bannerp from "./Bannerp.jsx";
+//import TendenciasHoy from "./TendenciasHoy.jsx"
+//<Bannerp/>
+//<TendenciasHoy/>
 function App() {
   return (
     <div>
       <Menu />
-      <Bannerp/>
-      <TendenciasHoy/>
     </div>
   );
 }
--- block/src/Menu.jsx
+++ block/src/Menu.jsx
@@ -1,21 +1,56 @@
-import "./App.css";
+import { useState } from 'react';
+import './Css/Menu.css';
 
 const Menu = () => {
+  // Estado para controlar si el menú está abierto o cerrado
+  const [menuAbierto, setMenuAbierto] = useState(false);
+
+  // Función para alternar entre abrir y cerrar el menú
+  const abrirOcerrar = () => {
+    setMenuAbierto(!menuAbierto);//cambia el estado atravez del signo
+
+  };
+
   return (
-    <div id="menu" className="menuprincipal">
-      <div className="contenedor1">
-      <img src="src\assets\logo1x.png" />
-      <h1>Inicio</h1>
-      <h1>Peliculas</h1>
-      <h1>Series</h1>
-      <h1>Actores</h1>
-    </div>
-      <form>
-        <input type="text" placeholder="Buscar serie o pelicula"></input>
-        <button type="submit"></button>
-      </form>
-    </div>
+    <header className="contenedorheader">
+      <nav className="topnav">
+        <a href="#" className="logo">
+          <img src="src\assets\logo1x.png" alt="Logo" />
+        </a>
+        <button className="abrir-menu" onClick={abrirOcerrar}>
+          Open
+        </button>
+        <ul className={`menu ${menuAbierto ? 'menu-abierto' : false}`}>
+          {/* menuAbierto = true; anadira la clase "menu-abierto"  
+          
+          cuando apretamos el boton X menuAbierto =false 
+          */}
+
+          <button className="cerrar-menu" onClick={abrirOcerrar}>
+            X
+          </button>
+          <li>
+            <a href="#Inicio">Inicio</a>
+          </li>
+          <li>
+            <a href="#Peliculas">Peliculas</a>
+          </li>
+          <li>
+            <a href="#Series">Series</a>
+          </li>
+          <li>
+            <a href="#Actores">Actores</a>
+          </li>
+          <li>
+            <form>
+              <input type="text" placeholder="Buscar serie o pelicula" />
+              <button type="submit"></button>
+            </form>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
-export default Menu
+export default Menu;

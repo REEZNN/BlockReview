2a9a81897f20fcd117d12708ed04e910324efe6f
REEZNN <johanstation15@gmail.com>
2024-03-23 12:25:27 -0300

    ficha

--- block/src/App.jsx
+++ block/src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css";
-import Menu from "./Menu.jsx";
-import Bannerp from "./Bannerp.jsx";
-import TendenciasHoy from "./TendenciasHoy.jsx";
-import Encines from "./Encines.jsx"
-//<Bannerp/>
-//<TendenciasHoy/>
+//import Menu from "./Menu.jsx";
+//import Bannerp from "./Bannerp.jsx";
+//import TendenciasHoy from "./TendenciasHoy.jsx";
+//import Encines from "./Encines.jsx"
+import Fichapelicula from "./FIchapelicula.jsx";
+
+//<Menu />
+//<Bannerp />
+//<TendenciasHoy />
+//<Encines/>
+
 function App() {
   return (
     <div>
-      <Menu />
-      <Bannerp />
-      <TendenciasHoy />
-      <Encines/>
+      <Fichapelicula/>
     </div>
   );
 }
---
+++ block/src/FIchapelicula.jsx
@@ -0,0 +1,12 @@
+import "./Css/Fichapelicula.css";
+import Menu from "./Menu.jsx";
+
+const Fichapelicula = () => {
+  return (
+    <section>
+      <Menu />
+      
+    </section>
+  );
+};
+export default Fichapelicula;
